77a58ff4f427cef3849d72329ad7f6fe1b740235
Khalezin Andrey <66243527+KhalezinAndrey89@users.noreply.github.com>
2024-01-26 19:02:23 +0300

    TASK-4: add UserController and method getUserById

---
+++ src/main/java/ru/javabegin/micro/demo/petproject/controller/UserController.java
@@ -0,0 +1,28 @@
+package ru.javabegin.micro.demo.petproject.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import ru.javabegin.micro.demo.petproject.entity.User;
+import ru.javabegin.micro.demo.petproject.service.UserServiceImpl;
+
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api")
+@RequiredArgsConstructor
+public class UserController {
+
+    private final UserServiceImpl userService;
+
+    @GetMapping("/user/{id}")
+    public ResponseEntity<Optional<User>> getUserById(@PathVariable("id") Long id) {
+        return ResponseEntity.ok(userService.findUserById(id));
+    }
+
+
+
+}
--- src/main/java/ru/javabegin/micro/demo/petproject/service/UserServiceImpl.java
+++ src/main/java/ru/javabegin/micro/demo/petproject/service/UserServiceImpl.java
@@ -2,7 +2,10 @@ package ru.javabegin.micro.demo.petproject.service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import ru.javabegin.micro.demo.petproject.entity.User;
 import ru.javabegin.micro.demo.petproject.repository.UserRepository;
+
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -10,6 +13,8 @@ public class UserServiceImpl {
 
     private final UserRepository userRepository;
 
-
+    public Optional<User> findUserById(Long id) {
+        return userRepository.findById(id);
+    }
 
 }
