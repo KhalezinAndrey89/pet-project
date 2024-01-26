0b6599e66830b01cf6d13a32c195053d26be4deb
Khalezin Andrey <66243527+KhalezinAndrey89@users.noreply.github.com>
2024-01-26 19:20:01 +0300

    TASK-5: add sql insert user and role

---
+++ src/main/resources/sql/InsertUser.sql
@@ -0,0 +1,14 @@
+INSERT INTO roles (name) VALUES ('Admin'), ('User');
+
+INSERT INTO users (username, firstname, lastname, password, role_id)
+VALUES
+    ('john.smith@example.com', 'John', 'Smith', 'password123', 1),
+    ('jane.doe@example.com', 'Jane', 'Doe', 'password456', 2),
+    ('michael.johnson@example.com', 'Michael', 'Johnson', 'password789', 2),
+    ('emily.williams@example.com', 'Emily', 'Williams', 'passwordabc', 2),
+    ('david.brown@example.com', 'David', 'Brown', 'passworddef', 2),
+    ('sarah.davis@example.com', 'Sarah', 'Davis', 'passwordghi', 2),
+    ('daniel.miller@example.com', 'Daniel', 'Miller', 'passwordjkl', 2),
+    ('olivia.wilson@example.com', 'Olivia', 'Wilson', 'passwordmno', 2),
+    ('matthew.moore@example.com', 'Matthew', 'Moore', 'passwordpqr', 2),
+    ('sophia.taylor@example.com', 'Sophia', 'Taylor', 'passwordstu', 2);
